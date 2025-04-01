da7b4e1a35a96053989190d8f5945a2536b43280
saicastic <saiprithvisuperior@gmail.com>
2025-04-01 23:54:43 +0530

    fix

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 
 import FeaturesSection from "@/components/homesections/FeaturesSection";
 import HomeAboutus from "@/components/homesections/HomeAboutus";
+import HomeDescription from "@/components/homesections/HomeDescription";
 import HomeHero from "@/components/homesections/HomeHero";
 import ZoomParallax from "@/components/homesections/ZoomParallax";
 import { ReactLenis } from "lenis/react";
@@ -9,11 +10,12 @@ import { ReactLenis } from "lenis/react";
 export default function Home() {
   return (
     <ReactLenis root>
-      <main>
+      <main className="pb-6">
         <HomeHero />
         <FeaturesSection />
         <ZoomParallax />
         <HomeAboutus />
+        <HomeDescription />
       </main>
     </ReactLenis>
   );
